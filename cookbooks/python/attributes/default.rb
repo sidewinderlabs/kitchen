bdb1e95b9a0f63bf54d7c05a76a5fbeaa0dc4e09
Mark Birbeck <mark.birbeck@gmail.com>
2012-01-16 14:24:13 +0000

    Import python version 1.0.2

--- cookbooks/python/attributes/default.rb
+++ cookbooks/python/attributes/default.rb
@@ -21,7 +21,7 @@
 default['python']['install_method'] = 'package'
 
 default['python']['url'] = 'http://www.python.org/ftp/python'
-default['python']['version'] = '2.7'
+default['python']['version'] = '2.7.1'
 default['python']['checksum'] = '80e387bcf57eae8ce26726753584fd63e060ec11682d1145af921e85fd612292'
 default['python']['prefix_dir'] = '/usr/local'
 
